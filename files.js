9bda825e86cf995646f8a41efcfbc819a08c71a3
Phillip Tran <ptrandev@protonmail.com>
2020-06-26 13:06:18 -0400

    commit

--- files.js
+++ files.js
@@ -1,10 +1,38 @@
 const fs = require("fs");
 
 // reading files
-fs.readFile()
+// fs.readFile("./docs/blog1.txt", (err, data) => {
+//   if (err) console.error(err);
+
+//   console.log(data.toString());
+// });
 
 // writing files
+// fs.writeFile("./docs/blog1.txt", "hello world", () => {
+//   console.log("file was written");
+// });
+
+// fs.writeFile("./docs/blog2.txt", "hello again", () => {
+//   console.log("file was written");
+// });
 
 // directories
+// if (!fs.existsSync('./assets')) {
+//   fs.mkdir("./assets", (err) => {
+//     if (err) return console.error(err);
+//     console.log("folder created");
+//   });
+// } else {
+//   fs.rmdir("./assets", (err) => {
+//     if (err) return console.error(err);
+//     console.log("folder deleted");
+//   })
+// }
 
 // deleting files
+if (fs.existsSync('./docs/deleteme.txt')) {
+  fs.unlink('./docs/deleteme.txt', (err) => {
+    if (err) return console.error(err)
+    console.log("file deleted");
+  })
+}
